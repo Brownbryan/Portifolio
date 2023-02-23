ea4fb9996681a078b9fefd8f074d4d5b7b043d88
Brownbryan <muriukibryan11@gmail.com>
2023-02-23 09:57:38 +0300

    comit

---
+++ file.js
@@ -0,0 +1,32 @@
+/* <h1 class="full_name">Brian Muthee</h1> <br>
+       <h5 class="heading_about2">Web Developer/Web Designer</h5> <br>
+       <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim ex, pharetra ut tempor et, faucibus nec elit. Aliquam tincidunt, lorem quis laoreet viverra, ante odio fringilla nisl, vel vulputate tortor sem in nisl. Duis non vehicula magna. Suspendisse dapibus semper posuere. Nunc in velit viverra ante mollis sollicitudin. Vivamus vitae nisi ut lorem vestibulum feugiat eu scelerisque quam. Suspendisse potenti. Aliquam id placerat ipsum, sed pretium diam. Donec bibendum ligula eget nunc dignissim gravida. Praesent tristique sem in aliquet lacinia. Etiam lobortis velit felis, nec dictum augue efficitur ut. Cras venenatis, urna quis euismod lacinia, felis velit ultricies nibh, id viverra urna odio et ligula. Nulla et mi justo. Aliquam eget suscipit massa. Pellentesque blandit hendrerit sem at tempus.</p>
+    <ul class="a">
+      <li class="profile"><span class="profiles">From &nbsp &nbsp &nbsp &nbsp:</span> Nairobi, Kenya</li>
+      <li class="profile"><span class="profiles">Lives in &nbsp:</span> Nairobi</li>
+      <li class="profile"><span class="profiles">Gender &nbsp:</span> Male</li>
+      <li class="profile"><span class="profiles">Age &nbsp &nbsp &nbsp &nbsp &nbsp: </span>22</li>
+    </ul> */
+//     <section class="about">
+//     <div class="home-container">
+//       <div class="container">
+//         <div class="row">
+//           <div class="col-md-5 col-md-6">
+//             <div class="image-about">
+//               <img src="/img/bryan2.png" class="image_about" alt="img-about">
+//             </div>
+            
+//           </div>
+
+//            <div class="col-lg-7 col-md-6">
+//             <div class="about_content">
+//               <h5 class="heading_about">Brian Muthee</h5>
+//               <p class="para_about"> Web Developer|Web Designer</p>
+//               <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim ex, pharetra ut tempor et, faucibus nec elit. Aliquam tincidunt, lorem quis laoreet viverra, ante odio fringilla nisl, vel vulputate tortor sem in nisl. Duis non vehicula magna. Suspendisse dapibus semper posuere. Nunc in velit viverra ante mollis sollicitudin. Vivamus vitae nisi ut lorem vestibulum feugiat eu scelerisque quam. Suspendisse potenti. Aliquam id placerat ipsum, sed pretium diam. Donec bibendum ligula eget nunc dignissim gravida. Praesent tristique sem in aliquet lacinia. Etiam lobortis velit felis, nec dictum augue efficitur ut. Cras venenatis, urna quis euismod lacinia, felis velit ultricies nibh, id viverra urna odio et ligula. Nulla et mi justo. Aliquam eget suscipit massa. Pellentesque blandit hendrerit sem at tempus.</p>
+
+//             </div>
+//            </div>
+//         </div>
+//       </div>
+//     </div>
+//   </section>
